66a13eabf9271037a37ece3429c7a6c0d0b8dc0f
elisamongelli <elisamongelli00@gmail.com>
2025-11-01 18:12:52 +0100

    Insert queries for testing saved.

---
+++ backend/structure/test_db_values/programmi.sql
@@ -0,0 +1,7 @@
+INSERT INTO LKP_PROGRAMMI (ID_PROGRAMMA, VALORE_SEQUENZA_TEMPORALE_PROGRAMMA, NOME_PROGRAMMA, ORARIO_INIZIO_PROGRAMMA, ORARIO_FINE_PROGRAMMA)
+VALUES  ('65246103-ae4b-4e4d-baea-b4f9cbbb7203', 1, 'Notturno', '01:20:00', '02:10:00'),
+        ('b088846c-f6cd-4996-9185-bff7569b6dc2', 2, 'Mattutino', '08:30:00', '09:30:00'),
+        ('cc8b252e-cd5b-4ce9-8954-600c2e1f7929', 3, 'Intermedio mattutino', '11:30:00', '12:00:00'),
+        ('c0e5c1ea-413e-4af1-b514-a9b29d03fff0', 4, 'Primo pomeridiano', '15:00:00', '16:00:00'),
+        ('d9f70589-c4c9-419a-8e79-45d9a7203b40', 5, 'Secondo pomeridiano', '18:15:00', '18:50:00'),
+        ('d749a160-8880-417a-8ff8-df8e4da00a71', 6, 'Serale', '20:45:00', '21:00:00');
---
+++ backend/structure/test_db_values/stati.sql
@@ -0,0 +1,6 @@
+INSERT INTO LKP_STATI_PIANTE (ID_STATO, NOME_STATO, DESCRIZIONE_STATO)
+VALUES  ("BAGNATO", "Bagnato", "Il terreno è bagnato, con umidità compresa tra il 61% e il 95%, dunque necessita innaffiatura tra oltre 24h."),
+        ("SECCO", "Secco", "Il terreno è secco, con umidità compresa tra l'11% e il 30%, dunque necessita di innaffiatura entro le 8h."),
+        ("TROPPO_BAGNATO", "Troppo bagnato", "Il terreno è troppo bagnato, con umidità compresa tra il 96% e il 100%, dunque non necessita di innaffiatura."),
+        ("TROPPO_SECCO", "Troppo secco", "Il terreno è troppo secco, con umidità fino al 10%, dunque necessita di innaffiatura immediata."),
+        ("UMIDO", "Umido", "Il terreno è umido, con umidità compresa tra il 31% e il 60%, dunque necessita di innaffiatura entro le 24h.");
