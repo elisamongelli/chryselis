2355ea2d6f29708bb985e7ad9e76f76c36b3c0e2
elisamongelli <81107849+elisamongelli@users.noreply.github.com>
2026-04-26 17:09:30 +0200

    Issue #34: run.py file set for launching both api and frontend.

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -6,7 +6,7 @@ from api.api import api_blueprint
 
 # methods for creating and configuring the Flask app
 def create_app(config_filename):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../../assets/html', static_url_path='')
     app.config.from_object(config_filename)
     db.init_app(app)
     # /chryselis is the base path for all API endpoints
--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+import os
 from api import create_app
 
 
@@ -6,6 +7,13 @@ from api import create_app
 app = create_app("config")
 
 
+# route frontend
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
 # run the application
 if __name__ == "__main__":
-    app.run(debug=True)
+    flagDebug = os.environ.get('CHRYSELIS_FLASK_ENV') == 'development'
+    app.run(debug=flagDebug)
